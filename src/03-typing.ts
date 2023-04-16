23ec1e10b0985ceaecb3e5c21117dad3c0448f8a
Daniel <danielcast244@gmail.com>
2023-04-16 15:13:03 -0500

    adelanto de estudio TS, voy por boolean

---
+++ src/03-typing.ts
@@ -0,0 +1,13 @@
+(() => {
+  let myProductName = "Product 1"
+  let myProductPrice = 1234;
+
+  myProductName = "cambio";
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed()
+
+  const myProductoStock = 1000;
+  const myProductName2 = "Product 1";
+})();
+
---
+++ src/04-numbers.ts
@@ -0,0 +1,32 @@
+(() => {
+  let productPrice = 1000;
+  productPrice = 12;
+  console.log('productoPrice', productPrice)
+
+  let customerAge: number = 25;
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge)
+
+  // let productInStock: number;
+  // console.log('productoInStock', productInStock)
+  // if(productInStock > 10){
+  //   console.log('is greater')
+  // }
+
+  let discount = parseInt('100')
+  console.log('discount', discount)
+  if(discount <= 200){
+    console.log('Apply')
+  }else{
+    console.log('not apply')
+  }
+
+  let hex = 0xFFF;
+  console.log('hex', hex)
+
+  let bin = 0b1010;
+  console.log('bin', bin)
+
+  const myNumber: Number = 10;
+
+})();
